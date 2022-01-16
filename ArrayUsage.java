a4fc2a920d9794c29ce1aba80b10e4bc8155eb47
jiaxiangu <gujiaxian1997@gmail.com>
2022-01-16 18:24:15 +0900

    add algo4 library

--- ArrayUsage.java
+++ ArrayUsage.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ArrayUsage {
+public class ArrayUsage {
+    public static void main(String[] args) {
+
+        double[] a;
+        a = new double[5];
+        for (int i = 0; i < N; i++) {
+            a[i] = 0.0;
+        }
+
+        /* initialize 2-d array */
+        double [][] a = new double[5][6];
+
+
+    }
 }
